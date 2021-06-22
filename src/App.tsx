0688531a7865bbf019bb35eea6ea542fb36448c6
Rafael Aragão <rafaelfaragao@gmail.com>
2021-06-21 23:08:29 -0300

    Criadas páginas Home e NewRoom, install react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import { Button } from "./components/Button";
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
 
 function App() {
   return (
-    <div>
-      <Button>Clique Aqui</Button>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-    </div>
+    <NewRoom />
   );
 }
 
