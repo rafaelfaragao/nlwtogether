801c53f8077d90f49f5604b1f92dac584a2db602
Rafael Aragão <rafaelfaragao@gmail.com>
2021-06-23 19:34:00 -0300

    Estruturando sala de perguntas, incluir e listar

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,25 +1,88 @@
+import { useEffect } from 'react';
+import { FormEvent, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { string } from 'yargs';
 
 import logoImg from '../assets/images/logo.svg';
 
 import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode'; 
+import { useAuth } from '../hooks/useAuth';
+import { database } from '../services/firebase';
 
 import '../styles/room.scss';
+
+type FirebaseQuestions = Record<string, {
+  author: {
+    name: string;
+    avatar: string;
+  }
+  content: string;
+  isAnswered: boolean;
+  isHighLighted: boolean;
+}>
 
 type RoomParams = {
   id: string;
 }
 
 export function Room () {
+  const { user } = useAuth();
   const params = useParams<RoomParams>();
+  const [newQuestion, setNewQuestion] = useState('');
+  
+  const roomId = params.id;
+
+  useEffect(()=>{
+    const roomRef = database.ref(`rooms/${roomId}`);
+
+    roomRef.once('value', room => {
+      const databaseRoom = room.val();
+      const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
+      const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value])=> {
+        return {
+          id: key,
+          content: value.content,
+          author: value.author,
+          isHighLighted: value.isHighLighted,
+          isAnswered: value.isAnswered,
+        }
+      });
+    })
+  }, [roomId])
+
+  async function handleSendQuestion(event: FormEvent) {
+    event.preventDefault();
+
+    if(newQuestion.trim() === '') {
+      return;
+    }
+
+    if(!user) {
+      throw new Error('You must be logged in');
+    }
+
+    const question = {
+      content: newQuestion,
+      author: {
+        name: user.name,
+        avatar: user.avatar
+      },
+      isHighLighted: false,
+      isAnswered: false
+    }
+
+    await database.ref(`rooms/${roomId}/questions`).push(question);
+
+    setNewQuestion('');
+  }
 
   return (
     <div id="page-room">
       <header>
         <div className="content">
           <img src={logoImg} alt="Letmeask"/>
-          <RoomCode code={params.id}/>
+          <RoomCode code={roomId}/>
         </div>
       </header>
 
@@ -29,14 +92,23 @@ export function Room () {
           <span>4 perguntas</span>
         </div>
 
-        <form>
+        <form onSubmit={handleSendQuestion}>
           <textarea
             placeholder="O que você quer perguntar"
+            onChange={event => setNewQuestion(event.target.value)}
+            value={newQuestion}
           />
 
           <div className="form-footer">
-            <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
-            <Button type="submit">Enviar Pergunta</Button>
+            { user ? (
+              <div className="user-info">
+                <img src={user.avatar} alt={user.name} />
+                <span>{user.name}</span>
+              </div>
+            ) : (
+              <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
+            )}
+            <Button type="submit" disabled={!user}>Enviar Pergunta</Button>
           </div>
         </form>
       </main>
